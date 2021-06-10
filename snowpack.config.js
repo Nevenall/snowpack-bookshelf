86c77154af1648aa244739daa58f05581144a22d
Dan Behlings <nevenall@hotmail.com>
2021-06-09 21:33:44 -0700

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,18 +4,19 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
    mount: {
-      /* ... */
+      public: { url: '/', static: true, resolve: false },
+      src: { url: '/dist' },
    },
    plugins: [
       '@snowpack/plugin-svelte'
    ],
    packageOptions: {
-      /* ... */
+
    },
    devOptions: {
-      /* ... */
+      port: 8080
    },
    buildOptions: {
-      /* ... */
+
    },
 };
